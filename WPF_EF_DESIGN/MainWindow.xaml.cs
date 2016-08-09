c3532f35e91123d01cce861655ab5ae734a85159
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-08-09 16:53:36 -0300

    wcf

--- WPF_EF_DESIGN/MainWindow.xaml.cs
+++ WPF_EF_DESIGN/MainWindow.xaml.cs
@@ -21,120 +21,102 @@ namespace WPF_EF_DESIGN
     /// </summary>
     public partial class MainWindow : Window
     {
-        ClientesEntities _context = new ClientesEntities();
-        ObservableCollection<Clientes> cliLista = new ObservableCollection<Clientes>();
+        //ClientesEntities _context = new ClientesEntities();
+        //ObservableCollection<Clientes> cliLista = new ObservableCollection<Clientes>();
+        ServiceReference1.Service1Client wcfService = new ServiceReference1.Service1Client();
 
         public MainWindow()
         {
             InitializeComponent();
-            foreach (var c in _context.Clientes)
+            /*foreach (var c in _context.Clientes)
             {
                 cliLista.Add(c);
             }
-            listBox1.ItemsSource = cliLista;
+            listClients.ItemsSource = cliLista;*/
+            //cliLista = wcfService.SearchClient();
+            listClients.ItemsSource = wcfService.SearchClient();
         }
 
-        private void AdicionaTabContato(Contatos c)
+        private void AdicionaTabContato(ServiceReference1.Contatos c)
         {
-            TabItem item = new TabItem();
+            /*TabItem item = new TabItem();
             ContatoControl ctrl = new ContatoControl();
             ctrl.DataContext = c;
             item.Content = ctrl;
             item.Header = c.Id.ToString();
-            tabControl1.Items.Add(item);
-            tabControl1.SelectedItem = item;
+            tabControlContacts.Items.Add(item);
+            tabControlContacts.SelectedItem = item;*/
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void NewClient_Click(object sender, RoutedEventArgs e)
         {
-            Clientes cli = new Clientes() { Nome = "Não Identificado" };
+            /*Clientes cli = new Clientes() { Nome = "Não Identificado" };
             _context.Clientes.Add(cli);
-            cliLista.Add(cli);
+            cliLista.Add(cli);*/
+            wcfService.CreateClient();
+            //listClients.ItemsSource = wcfService.SearchClient();
         }
 
-        private void button2_Click(object sender, RoutedEventArgs e)
+        private void DeleteClient_Click(object sender, RoutedEventArgs e)
         {
-            if(listBox1.SelectedItem != null)
+            if(listClients.SelectedItem != null)
             {
-                Clientes cli = (Clientes)listBox1.SelectedItem;
+                /*Clientes cli = (Clientes)listClients.SelectedItem;
                 cli.Contatos.Clear();
-                _context.Clientes.Remove((Clientes)listBox1.SelectedItem);
-                cliLista.Remove((Clientes)listBox1.SelectedItem);
+                //_context.Clientes.Remove((Clientes)listClients.SelectedItem);
+                //cliLista.Remove((Clientes)listClients.SelectedItem);
+                _context.Clientes.Remove(cli);
+                cliLista.Remove(cli);*/
+                wcfService.DeleteClient((ServiceReference1.Clientes)listClients.SelectedItem);
             }
         }
 
-        private void button3_Click(object sender, RoutedEventArgs e)
+        private void Save_Click(object sender, RoutedEventArgs e)
         {
-            _context.SaveChanges();
+            //_context.SaveChanges();
+            wcfService.UpdateClient();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NewContact_Click(object sender, RoutedEventArgs e)
         {
-            if(listBox1.SelectedItem != null)
+            if(listClients.SelectedItem != null)
             {
-                Clientes cli = (Clientes)listBox1.SelectedItem;
+                /*Clientes cli = (Clientes)listClients.SelectedItem;
                 Contatos con = new Contatos() { Cliente = cli.Id, Nome = "Não identificado" };
                 cli.Contatos.Add(con);
-                AdicionaTabContato(con);
+                AdicionaTabContato(con);*/
+                wcfService.CreateContact((ServiceReference1.Clientes)listClients.SelectedItem);
             }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void DeleteContact_Click(object sender, RoutedEventArgs e)
         {
-            if(listBox1.SelectedItem != null && tabControl1.SelectedItem != null)
+            if (listClients.SelectedItem != null && tabControlContacts.SelectedItem != null)
             {
-                Clientes cli = (Clientes)listBox1.SelectedItem;
-                Contatos con = (Contatos)((ContatoControl)((TabItem)tabControl1.SelectedItem).Content).DataContext;
+                /*Clientes cli = (Clientes)listClients.SelectedItem;
+                Contatos con = (Contatos)((ContatoControl)((TabItem)tabControlContacts.SelectedItem).Content).DataContext;
                 cli.Contatos.Remove(con);
-                tabControl1.Items.Remove(tabControl1.SelectedItem);
-                if(tabControl1.Items.Count > 0)
+                tabControlContacts.Items.Remove(tabControlContacts.SelectedItem);
+                if(tabControlContacts.Items.Count > 0)
                 {
-                    tabControl1.SelectedIndex = 0;
-                }
+                    tabControlContacts.SelectedIndex = 0;
+                }*/
+                wcfService.DeleteContact((ServiceReference1.Clientes)listClients.SelectedItem, (ServiceReference1.Contatos)tabControlContacts.SelectedItem);
             }
         }
 
-        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ListClients_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            tabControl1.Items.Clear();
-            if(listBox1.SelectedItem != null)
+            tabControlContacts.Items.Clear();
+            if(listClients.SelectedItem != null)
             {
-                Clientes cli = (Clientes)listBox1.SelectedItem;
+                /*Clientes cli = (Clientes)listClients.SelectedItem;
                 foreach(var c in cli.Contatos)
                 {
                     AdicionaTabContato(c);
-                }
+                }*/
+                wcfService.SearchContact((ServiceReference1.Clientes)listClients.SelectedItem);
             }
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_4(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_5(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
